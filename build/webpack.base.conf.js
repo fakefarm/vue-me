2bf0c19e24df900fb54394072ab12ef8f3f305d9
fakefarm <dave@woodalls.me>
2017-10-22 13:36:28 -0400

    hack gh-pages

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -22,7 +22,7 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.vue', '.json'],
     alias: {
-      'vue$': 'vue/dist/vue.esm.js',
+      'vue$': 'vue/docs/vue.esm.js',
       '@': resolve('src'),
     }
   },
